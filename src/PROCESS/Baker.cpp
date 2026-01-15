ce48a08e4e757f7a5c757c23ba7227a1e0b01fa9
Kris <71553569+Krisuniooo@users.noreply.github.com>
2026-01-15 23:00:07 +0100

    WIP: Baker process

--- src/PROCESS/Baker.cpp
+++ src/PROCESS/Baker.cpp
@@ -1,8 +1,12 @@
 #include <unistd.h>
 #include <iostream>
 #include <signal.h>
+#include <string.h>
 #include "../../include/Config.h"
 #include "../../include/Utils.h"
+#include "../../include/Logger.h"
+#include "../../include/IPC/SharedMemory.h"
+#include "../../include/IPC/Semaphore.h"
 
 static void handlerSigOne(int sig) {
 	std::cout << "TEST\n";
@@ -16,14 +20,40 @@ int main() {
 	sigaction(SIGUSR2, &sa, NULL);
 	
 	std::cout << "TEST2\n";
+	
+	SharedData* data = static_cast<SharedData*>(SHAREDMEMORY::attach());
+	int semid = SEMAPHORE::getID();
 
 	while(true) {
 		int time = UTILS::getRandom(BAKE_MIN_TIME, BAKE_MAX_TIME) * SIMULATION_MINUTE;
-		usleep(time);
+		usleep(static_cast<int>(time*SIMULATION_MINUTE));
 		
-		//todo
-		break;
+		if(data->is_evacuation)
+			break;
+			
+		for(int i = 0; i<PRODUCTS; i++) {
+			int amount = UTILS::getRandom(BAKE_MIN_PRODUCTS, BAKE_MAX_PRODUCTS);
+			
+			
+			if(SEMAPHORE::lock(data->trays[i].sem_num)) {
+				SEMAPHORE::lock(static_cast<int>(SemaphoreTypes::SHARED_DATA_MUTEX));
+				
+				if((data->trays[i].in_stock + amount) > data->trays[i].max_stock) {
+					LOGGER::log("Baker: added " + std::to_string(data->trays[i].max_stock - data->trays[i].in_stock) + ", trashed " + std::to_string(data->trays[i].in_stock + amount - data->trays[i].max_stock) +  " " + data->trays[i].label + ", new value: " + std::to_string(data->trays[i].in_stock));
+					data->trays[i].in_stock = data->trays[i].max_stock;
+				} else {
+					LOGGER::log("Baker: added " + std::to_string(amount) + " " + data->trays[i].label + ", new value: " + std::to_string(data->trays[i].in_stock));
+					data->trays[i].in_stock = data->trays[i].in_stock + amount;
+				}
+				
+				
+				SEMAPHORE::unlock(static_cast<int>(SemaphoreTypes::SHARED_DATA_MUTEX));
+				SEMAPHORE::unlock(data->trays[i].sem_num);
+			}
+		}
 	}
+	
+	SHAREDMEMORY::detach();
 	
 	return 0;
 }
